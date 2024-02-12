b5cf1a872c538195c88c736df5a149b1804aa699
Luis <101060519+LuisFun@users.noreply.github.com>
2024-02-12 21:06:59 +0900

    fix: url

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import type { Context } from 'discord-hono'
 import { DiscordHono, Command, CommandOption } from 'discord-hono'
 
 const imageHandler = async (c: Context) => {
-  const image = await fetch('https://github.com/microsoft/fluentui-emoji/blob/main/assets/Fire/3D/fire_3d.png')
+  const image = await fetch('https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Fire/3D/fire_3d.png')
   const blob = new Blob([await image.arrayBuffer()])
   await c.followup({ content: c.command.values[0] }, { blob, name: 'image.png' })
 }
 
 export const commands = [
-  new Command('ping', 'response pong').resText('Pong'),
+  new Command('ping', 'response pong').resText('Pong!!'),
   new Command('image', 'response image file with text')
     .option(new CommandOption('content', 'response text').required())
     .resDefer(imageHandler),
