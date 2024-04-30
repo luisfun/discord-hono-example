7dd2c5a9a7ea293aa0048ceb3a8b3fef6a2cb49e
Luis <101060519+LuisFun@users.noreply.github.com>
2024-04-30 17:19:04 +0900

    fix: register

--- src/register.ts
+++ src/register.ts
@@ -1,16 +1,16 @@
-import dotenv from 'dotenv'
-import process from 'node:process'
+import { config } from 'dotenv'
+import { env } from 'node:process'
 import { Command, Option, register } from 'discord-hono'
-dotenv.config({ path: '.dev.vars' })
+config({ path: '.dev.vars' })
 
 const commands = [
   new Command('hello', 'response world'),
   new Command('help', 'response help').options(new Option('text', 'with text')),
 ]
 
-await register(
+register(
   commands,
-  process.env.DISCORD_APPLICATION_ID,
-  process.env.DISCORD_TOKEN,
-  //process.env.DISCORD_TEST_GUILD_ID,
+  env.DISCORD_APPLICATION_ID,
+  env.DISCORD_TOKEN,
+  //env.DISCORD_TEST_GUILD_ID,
 )
