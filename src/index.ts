f10b1ed14a96024fdd1052f354d28f4cc4689ed7
Luis <101060519+LuisFun@users.noreply.github.com>
2024-08-11 08:58:03 +0900

    style: run fix

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = new DiscordHono()
     c.res({
       content: `text: ${c.var.text}`,
       components: new Components().row(
-        new Button('https://discord-hono.luis.fun', 'Docs', "Link"),
+        new Button('https://discord-hono.luis.fun', 'Docs', 'Link'),
         new Button('delete-self', 'Delete', 'Secondary').emoji({ name: '🗑️' }),
       ),
     }),
